8e4469afb851d9a855b7decd1b02892437b07658
Ansh Kataria <anshkataria28@gmail.com>
2023-04-23 23:40:37 +0530

    updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
           </div>
           <div className="hero-first-left-text">
             <p>
-              Lorem ipsum dolor sitmnn amet, consectetur adipiscing elit,
+              Lorem ipsum dolor sitmnnfdsjkg amet, consectetur adipiscing elit,
               <br /> sed do eiusmod tempor kgsafsfgsds et dolore <br />{" "}
               nisl tincidunt eget. Lectus mauris eros in vitae .
             </p>
